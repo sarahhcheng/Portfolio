fdd0f57ef702c55da622d424b83c39ba403f1ef3
sarahhcheng <sarahcheng40@gmail.com>
2023-12-28 22:22:26 -0800

    add company logos to my work experiences

--- src/components/experiences/Work.js
+++ src/components/experiences/Work.js
@@ -1,19 +1,25 @@
 import React from "react";
 
 function Work(props) {
+  const { title, company, location, time, description, image } = props;
   return (
     <div className="job-container">
-      <h3 className="job-title">{props.title}</h3>
-      <div className="job-details">
-        <p className="company-title">{props.company} </p>
-        <p>
-          <b>|</b>
-        </p>
-        <p>{props.location}</p>
+      <div className="details-container">
+        <img src={image} class="company-logos" />
+        <div className="job-info-container">
+          <h3 className="job-title">{title}</h3>
+          <div className="job-details">
+            <p className="company-title">{company} </p>
+            <p>
+              <b>|</b>
+            </p>
+            <p>{location}</p>
+          </div>
+          <p className="time">{time}</p>
+        </div>
       </div>
-      <p className="time">{props.time}</p>
       <div className="job-description">
-        <p>{props.description}</p>
+        <p>{description}</p>
       </div>
     </div>
   );
--- src/components/experiences/experiences-page.js
+++ src/components/experiences/experiences-page.js
@@ -7,7 +7,6 @@ function ExperiencePage() {
   return (
     <div className="container">
       <h1 className="work-experience-title">Experiences</h1>
-
       <div className="experiences">
         <Work
           title="Software Engineer Intern"
@@ -15,13 +14,15 @@ function ExperiencePage() {
           location="Vancouver, BC"
           time="Sept. 2023 - Dec. 2023"
           description="I was in the Online Services team for the Coalition Studio. I helped build internal tools to help track service outages."
+          image="http://purepng.com/public/uploads/large/purepng.com-microsoft-logo-iconlogobrand-logoiconslogos-251519939091wmudn.png"
         />
         <Work
           title="Software Developer Intern"
           company="SAP"
           location="Vancouver, BC"
           time="Jan. 2023 - Aug. 2023"
-          description="In my 8 month term at SAP, I had the opportunity to impact over 400,000 users as I worked on the SAP Analytics Cloud. "
+          description="In my 8 month term at SAP, I had the opportunity to impact over 400,000 users as I worked on the SAP Analytics Cloud."
+          image="https://logos-download.com/wp-content/uploads/2016/08/SAP_logo.png"
         />
         <Work
           title="Security Response Program Manager Intern"
@@ -29,6 +30,15 @@ function ExperiencePage() {
           location="Waterloo, ON"
           time="Sept. 2022 - Dec. 2022"
           description="I worked on the BlackBerry Security Incident Response Team and helped manage a large scale project that tracked open-source software vulnerabilities used in BlackBerry products."
+          image="https://pluspng.com/img-png/blackberry-logo-png-blackberry-limited-logo-1200x826.png"
+        />
+        <Work
+          title="Undergraduate Teaching Assistant"
+          company="UBC"
+          location="Vancouver, BC"
+          time="Sept. 2022 - April 2023"
+          description="I worked on the BlackBerry Security Incident Response Team and helped manage a large scale project that tracked open-source software vulnerabilities used in BlackBerry products."
+          image="https://avatars.githubusercontent.com/u/30203266?s=280&v=4"
         />
       </div>
     </div>
