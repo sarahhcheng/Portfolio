92033bf49880685a02fcddb08c431375550fdee8
sarahhcheng <sarahcheng40@gmail.com>
2023-12-29 21:24:45 -0800

    add projects to projects page and add project photos folder

--- src/components/ProjectCards/Card.js
+++ src/components/ProjectCards/Card.js
@@ -1,14 +1,38 @@
 import "./card.css";
 
 function Card(props) {
+  const { title, description, tech, image } = props;
   return (
     <>
       <div class="project-item">
-        <div class="project-img card2"></div>
+        <div className="project-image">
+          {" "}
+          <img
+            alt="image"
+            className="image"
+            src={`project-images/${image}.png`}
+          ></img>
+        </div>
         <div class="project-text">
-          <h3>{props.title}</h3>
-          <p class="description">{props.description}</p>
-          <p class="technology">HTML &nbsp; CSS &nbsp; Javascript</p>
+          <h3>{title}</h3>
+          <p class="description">{description}</p>
+          <p class="tech">{tech}</p>
+        </div>
+        <div class="project-buttons">
+          <a
+            href="https://tdanielles.github.io/boba-tracks/"
+            target="_blank"
+            class="demo"
+          >
+            <i class="fa-solid fa-arrow-up-right-from-square"></i>
+          </a>
+          <a
+            href="https://github.com/sarahhcheng/boba-tracks"
+            target="_blank"
+            class="code"
+          >
+            <i class="fa-brands fa-github-alt"></i>
+          </a>
         </div>
       </div>
     </>
--- src/components/ProjectPage/ProjectPage.js
+++ src/components/ProjectPage/ProjectPage.js
@@ -1,8 +1,38 @@
+import "./ProjectPage.css";
+import Card from "../ProjectCards/Card";
+
 function ProjectPage() {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div className="project-page">
+      <h1 className="project-headline">Projects</h1>
+      <div className="project-container">
+        <Card
+          title="Labby"
+          description="• Built a lab management website at UBC Launch Pad with a team of 15 for BC Cancer for 100+ employees
+• Connected the frontend in React to send user input to the MySQL database"
+          tech="React, Node, Express, MySQL"
+          image="labby"
+        />
+        <Card
+          title="Enspo"
+          description=""
+          tech="React, Node, Express, OpenAI"
+          image="enspo"
+        />
+        <Card
+          title="Recipe Book"
+          tech="Java, Java Swing"
+          description="Hello"
+          image="recipebook"
+        />
+        <Card
+          title="BobaTracks"
+          tech="JavaScript, HTML, CSS"
+          image="bobatracks"
+        />
+        <Card title="AReward" tech="Figma, UX/UI" image="areward" />
+      </div>
+    </div>
   );
 }
 
