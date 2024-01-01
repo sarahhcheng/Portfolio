91d43c35a4bc189e6ba6a94ec2aa8ff2c1c0ce72
sarahhcheng <sarahcheng40@gmail.com>
2023-12-31 18:42:48 -0800

    add about me section

--- src/components/homepage/About/About.js
+++ src/components/homepage/About/About.js
@@ -3,8 +3,30 @@ import "./About.css";
 function About() {
   return (
     <div className="about-container">
-      About Me
-      <div>Here is a summary of me and experiences</div>
+      <h1 className="about-title">About Me</h1>
+      <div className="about-img-container">
+        <img src="me.png" className="about-img" />
+      </div>
+      <div className="about-description">
+        <ul>
+          <li>
+            Third year business and computer science student at{" "}
+            <span className="org">The University of British Columbia. </span>
+          </li>
+          <li>
+            Previous Software Engineer Intern at{" "}
+            <span className="org">Microsoft.</span>
+          </li>
+          <li>
+            Previous Software Developer Intern at{" "}
+            <span className="org">SAP. </span>
+          </li>
+          <li>
+            I also did a program management internship at{" "}
+            <span className="org">BlackBerry!</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
