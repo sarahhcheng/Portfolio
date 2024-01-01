850229054bc9962e14b434f2d42c0b69da13eee5
sarahhcheng <sarahcheng40@gmail.com>
2023-12-31 17:45:05 -0800

    fix styling for pages

---
+++ src/components/homepage/About/About.js
@@ -0,0 +1,12 @@
+import "./About.css";
+
+function About() {
+  return (
+    <div className="about-container">
+      About Me
+      <div>Here is a summary of me and experiences</div>
+    </div>
+  );
+}
+
+export default About;
--- src/components/homepage/Homepage.js
+++ src/components/homepage/Homepage.js
@@ -1,23 +1,29 @@
 import "./homepage.css";
 import Headline from "./Headline";
+import About from "./About/About";
 
 function Homepage() {
   return (
-    <div className="container">
-      <div className="main-content">
-        <h1 className="intro-text">Hi, I am Sarah</h1>
-        <div className="img-container">
-          {" "}
-          <img
-            src="https://cdn.dribbble.com/users/300776/screenshots/1937476/boba.gif"
-            class="intro__img"
-          />
+    <>
+      <div className="home-container">
+        <div className="center-align">
+          <div className="main-content">
+            <h1 className="intro-text">Hi, I am Sarah</h1>
+            <div className="img-container">
+              {" "}
+              <img
+                src="https://cdn.dribbble.com/users/300776/screenshots/1937476/boba.gif"
+                class="intro__img"
+              />
+            </div>
+          </div>
+          <div className="headline">
+            <Headline />
+          </div>
         </div>
       </div>
-      <div className="headline">
-        <Headline />
-      </div>
-    </div>
+      <About />
+    </>
   );
 }
 
